64e89d92f23b74edba00da4c41a15fa59fa82bb5
mikemrg17 <miguelangelmurga1234@gmail.com>
2021-01-28 05:08:16 -0600

    Funcionalidad principal

---
+++ index.js
@@ -0,0 +1,139 @@
+const readline = require('readline');
+const fs = require('fs');
+const { exit } = require('process');
+//const { spawn } = require('child_process');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+(function(){
+    console.log("\nBienvenido al programa del universo de cadenas");
+    console.log("\t1. Ingresar potencia manualmente");
+    console.log("\t2. Generar potencia aleatoria");
+    console.log("\t3. Salir del programa");
+    rl.question('Opción:' , (opcion) =>{
+        if(opcion == 1){
+            manual();
+        }else if(opcion == 2){
+            aleatoria();
+        }else{
+            exit();
+        }
+        //rl.close();
+    });
+})();
+
+function manual(){
+    fs.createWriteStream('output.txt');
+    fs.appendFileSync('output.txt', "U={\n", (error)=>{
+        if (error) console.log(`Error: ${error}`);
+    });
+    rl.question('Por favor inserta una potencia ', (potencia) =>{
+        let combinaciones = Math.pow(2,potencia);
+        let numeroDecimal;
+        let numeroBinario;
+        let banderaProceso;
+        for(numeroDecimal = 0; numeroDecimal <= combinaciones; numeroDecimal++){
+            numeroBinario = numeroDecimal.toString(2);
+            fs.appendFileSync('output.txt',"\t" +numeroBinario + "\n", (error)=>{
+                if (error) console.log(`Error: ${error}`);
+            });
+            console.log("numero binario: " + numeroBinario);
+            banderaProceso = proceso(numeroBinario, potencia);
+            if(banderaProceso == 1){
+                break;
+            }else{
+                continue;
+            }
+        }
+        fs.appendFile('output.txt', "}", (error)=>{
+            if (error) console.log(`Error: ${error}`);
+        });
+        menu2();
+    });
+}
+
+function aleatoria(){
+    fs.createWriteStream('output.txt');
+    fs.appendFileSync('output.txt', "U={\n", (error)=>{
+        if (error) console.log(`Error: ${error}`);
+    });
+    let potencia = Math.round(Math.random() * (1000-0) + 0);
+    console.log(`Potencia: ${potencia}`);
+    let combinaciones = Math.pow(2,potencia);
+    let numeroDecimal;
+    let numeroBinario;
+    let banderaProceso;
+    for(numeroDecimal = 0; numeroDecimal <= combinaciones; numeroDecimal++){
+        numeroBinario = numeroDecimal.toString(2);
+        fs.appendFileSync('output.txt',"\t" +numeroBinario + "\n", (error)=>{
+            if (error) console.log(`Error: ${error}`);
+        });
+        console.log("numero binario: " + numeroBinario);
+        banderaProceso = proceso(numeroBinario, potencia);
+        if(banderaProceso == 1){
+            break;
+        }else{
+            continue;
+        }
+    }
+    fs.appendFile('output.txt', "}", (error)=>{
+        if (error) console.log(`Error: ${error}`);
+    });
+    menu2();
+}
+
+
+function proceso(numeroBinario, potencia){
+    let bandera;
+    if(contarUnos(numeroBinario) == potencia){
+        bandera = 1;
+    }else{
+        bandera = 0;
+    }
+    return bandera;
+    //console.log("Numero de 1: " + contarUnos(numeroBinario));
+}
+
+function contarUnos(numeroBinario){
+    let contador = 0;
+    for(let i = 0; i < numeroBinario.length; i++){
+        if(numeroBinario[i] == "1"){
+            contador++;
+        }
+    }
+    return contador;
+}
+
+
+function menu1(){
+    console.log("\nBienvenido al programa del universo de cadenas");
+    console.log("\t1. Ingresar potencia manualmente");
+    console.log("\t2. Generar potencia aleatoria");
+    console.log("\t3. Salir del programa");
+    rl.question('Opción:' , (opcion) =>{
+        if(opcion == 1){
+            manual();
+        }else if(opcion == 2){
+            aleatoria();
+        }else{
+            exit();
+        }
+        //rl.close();
+    });
+}
+
+function menu2(){
+    console.log("\n¿Quieres volver a hacer un cálculo?");
+    console.log("\t1. Si");
+    console.log("\t2. No");
+    rl.question('Opción:' , (opcion) =>{
+        if(opcion == 1){
+            menu1();
+        }else{
+            exit();
+        }
+    });
+}
